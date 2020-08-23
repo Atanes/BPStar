a3b5d59e32f46af756888c210972aad581560902
Atanes - Dell <aatan@DESKTOP-RU0ST02>
2020-08-22 22:23:33 -0300

    Exclusão de método não utilizado no HomeController e inclusão de sub titulo na página inicial

--- src/main/java/com/bpstar/control/HomeController.java
+++ src/main/java/com/bpstar/control/HomeController.java
@@ -2,7 +2,6 @@ package com.bpstar.control;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
@@ -10,10 +9,5 @@ public class HomeController {
 	@GetMapping("/")
 	public String inicio() {
 		return "inicio";
-	}
-	
-	@RequestMapping("/teste")
-	public String teste() {
-		return "teste";
 	}
 }
